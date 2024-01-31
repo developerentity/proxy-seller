50ef3b0f0b5be70454f1ebb694af4828ac48c6e6
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-01-31 22:27:18 +0200

    style: 404 page layout was created

--- src/components/NoMatch/index.tsx
+++ src/components/NoMatch/index.tsx
@@ -1,12 +1,17 @@
-
+import { useNavigate } from "react-router-dom";
+import { BackButton, Message, NotFoundContainer } from "./styles";
 
 
 const NoMatch = () => {
 
+    const navigate = useNavigate();
+
     return (
-        <>
-        </>
-    )
+        <NotFoundContainer>
+            <Message>Page not found - 404</Message>
+            <BackButton onClick={() => navigate(-1)}>Go back</BackButton>
+        </NotFoundContainer>
+    );
 }
 
 export default NoMatch;
--- src/components/NoMatch/styles.ts
+++ src/components/NoMatch/styles.ts
@@ -1,5 +1,30 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
+export const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+`;
 
+export const Message = styled.h1`
+  font-size: 2rem;
+  color: #333;
+`;
+
+export const BackButton = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1rem;
+  color: white;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
 `;
