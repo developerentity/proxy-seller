e364d648fddecdb92ddf6cd1355ad48c8ed7b787
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-02-04 23:39:42 +0200

    feat: Suspense feature was added to Router

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes as Switch, Route } from "react-router-dom";
-
-import UsersComponent from "./components/Users";
-import UserAlbumsComponent from "./components/UserAlbums";
-import UserPostsComponent from "./components/UserPosts";
+import Loading from "./components/Loading";
 import NoMatch from "./components/NoMatch";
+
+const UsersComponent = lazy(() => import("./components/Users"));
+const UserAlbumsComponent = lazy(() => import("./components/UserAlbums"));
+const UserPostsComponent = lazy(() => import("./components/UserPosts"));
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" element={<UsersComponent />} />
-      <Route path="/:userId/albums" element={<UserAlbumsComponent />} />
-      <Route path="/:userId/posts" element={<UserPostsComponent />} />
-      <Route path="*" element={<NoMatch />} />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route path="/" element={<UsersComponent />} />
+        <Route path="/:userId/albums" element={<UserAlbumsComponent />} />
+        <Route path="/:userId/posts" element={<UserPostsComponent />} />
+        <Route path="*" element={<NoMatch />} />
+      </Switch>
+    </Suspense>
   );
 };
 
