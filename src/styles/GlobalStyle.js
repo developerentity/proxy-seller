a43e49ee828ec49f7e1eef54405763e69f6895af
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-02-05 02:35:34 +0200

    styles: minor changes in global styles

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -4,8 +4,20 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: Arial, sans-serif;
-  }
+    font-family: "San Francisco", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    "Roboto", "Helvetica Neue", Helvetica, sans-serif;
+    font-size: 1rem;
+    line-height: 1.65;
+    color: #333; 
+    line-height: 1;
+    background: #f0f0f0;
+  };
+  ol, ul {
+    list-style: none;
+  };
+  a:link {
+    text-decoration: none;
+  };
 `;
 
 export default GlobalStyle;
