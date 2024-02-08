e5700d08cb1d5ee050fc32f26710c076be0440cd
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-02-08 17:01:57 +0200

    feat: query params now is synchronizes according to sorn and search data

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,7 +1,23 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { debounce } from "lodash";
 
 export const useFilter = (items) => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const initialSearchTerm =
+    new URLSearchParams(location.search).get("search") || "";
+  const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
+
+  useEffect(() => {
+    const debouncedUpdateURL = debounce((newSearchTerm) => {
+      const searchParams = new URLSearchParams(location.search);
+      searchParams.set("search", newSearchTerm);
+      navigate(`?${searchParams.toString()}`, { replace: true });
+    }, 300);
+
+    debouncedUpdateURL(searchTerm);
+  }, [searchTerm, navigate, location.search]);
 
   const filteredItems = useMemo(() => {
     return items.filter((user) =>
--- src/hooks/useSort.js
+++ src/hooks/useSort.js
@@ -1,16 +1,29 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const useSort = (items) => {
-  const [sortOrder, setSortOrder] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const initialSortOrder = new URLSearchParams(location.search).get("sort");
+  const [sortOrder, setSortOrder] = useState(initialSortOrder);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    if (sortOrder) {
+      searchParams.set("sort", sortOrder);
+    } else {
+      searchParams.delete("sort");
+    }
+    navigate(`?${searchParams.toString()}`, { replace: true });
+  }, [sortOrder, navigate, location.search]);
 
   const sortedItems = useMemo(() => {
     if (!sortOrder) return items;
 
-    const itemsCopy = [...items].sort((a, b) => {
+    return [...items].sort((a, b) => {
       if (sortOrder === "asc") return a.username.localeCompare(b.username);
       else return b.username.localeCompare(a.username);
     });
-    return itemsCopy;
   }, [items, sortOrder]);
 
   const toggleSortOrder = () => {
